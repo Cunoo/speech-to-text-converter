d289e511e9eb4a453b82fe8346ebb45f6d49262d
matus mac <ccc@gmail.com>
2025-07-07 00:47:00 +0200

    modify App.js to AuthProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from './contexts/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 import Header from './components/Header/Header';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import Dashboard from './pages/Dashboard/Dashboard';
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        {/* Add your other pages here */}
-        <Route path="/register" element={<Register/>} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
