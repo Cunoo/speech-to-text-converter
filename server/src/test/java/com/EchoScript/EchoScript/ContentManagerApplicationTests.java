f534626130c4ca0f58815d528885bae2b0b09062
matus mac <ccc@gmail.com>
2025-08-03 12:08:10 +0200

    Rename whole project

---
+++ server/src/test/java/com/EchoScript/EchoScript/ContentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EchoScript.EchoScript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
